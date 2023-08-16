a3a608e5879efe5e455efe057f48b58f4cbf90d9
dev-tahsin7 <tahsinahmad2004@gmail.com>
2023-08-16 13:08:02 +0600

    Strings File

---
+++ Fundamental Recap/strings.py
@@ -0,0 +1,35 @@
+# Strings are Array :
+name = "Tahsin Ahmad"
+print(name[1]) 
+
+# looping 
+for i in name:
+    print(i) 
+
+# len()
+print(len(name))
+
+# check string 
+if "T" in name:
+    print("We got it")
+else:
+    pass
+
+# Slicing Strings
+
+print(name[2:5])
+print(name[:3])
+print(name[2:])
+print(name[-5:-2])
+
+# Strings Methods 
+
+print(name.upper())
+print(name.lower())
+print(name.replace("Ahmad", "Khan"))
+
+# There are lots of String Methods in Python 
+
+
+# It's just a revision code. Here I use only one variable called name 
+# and do all the things. Tahsin Ahmad !! 
