e3a45452277932f984771453521508d9106064e1
dev-tahsin7 <tahsinahmad2004@gmail.com>
2023-08-22 00:27:47 +0600

    update

--- Problem Solving/PS_DAY_1.py
+++ Problem Solving/PS_DAY_1.py
@@ -8,10 +8,10 @@ print this :
 
 # Solution:
 
-# for i in range(4):
-#     for j in range(i+1):
-#         print("#", end=" ")
-#     print()
+for i in range(4):
+    for j in range(i+1):
+        print("#", end=" ")
+    print()
 
 """
 # # # #
@@ -22,18 +22,18 @@ print this :
 
 # Solution:
 
-# for i in range(4,0,-1):
-#     for j in range(0,i):
-#         print("#", end=" ")
-#     print()
+for i in range(4,0,-1):
+    for j in range(0,i):
+        print("#", end=" ")
+    print()
 
 # Swaping two element in List 
 
 # Solution:
 
-# x = [1,2,4,3,5,6]
-# # We will swip 4 & 3
-# y = x[2]
-# x[2] = x[3]
-# x[3] = y
-# print(x)
+x = [1,2,4,3,5,6]
+# We will swip 4 & 3
+y = x[2]
+x[2] = x[3]
+x[3] = y
+print(x)
