94240592a3d4ceca8d33adb9beea8b9bcbec5e47
dev-tahsin7 <tahsinahmad2004@gmail.com>
2023-10-17 18:54:44 +0600

    new day-36 practice file

---
+++ Practice Program/day_36.py
@@ -0,0 +1,15 @@
+import calendar
+
+# 14. Find the factors of a number: 
+# num = int(input("Enter your number: "))
+
+# for i in range(1, num+1):
+#     if num % i == 0 :
+#         print(i)
+
+# 15. Display calender:
+# year = int(input("Enter year: "))
+# month = int(input("Enter month: "))
+
+# calendar = calendar.month(year,month)
+# print(calendar)
